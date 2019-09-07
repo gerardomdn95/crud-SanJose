30da6691434bcf61c734cb46cc92743c2d9f5edb
gerardomdn95 <gerardo@devf.mx>
2019-09-06 20:17:56 -0600

    cors

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const bodyParser = require("body-parser");
 const { router } = require("./routes/routes");
 const MONGOURI = require("./config/keys").mongoURI;
 const PORT = process.env.PORT || 4200;
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true})
     .then(() => console.log("Conectado a MongoDB 🚀"))
